84cdc4aeab3024d1f4d410d6ca0f42d75f5d101e
Andrew Monthero <andrewmonthero21@gmail.com>
2025-09-20 12:32:32 +0530

    added database setup

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import app from "./src/app.js";
 import { config } from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
 
-const startServer = () => {
+const startServer = async () => {
+  // Connect database
+  await connectDB();
+
   const port = config.port || 3000;
 
   app.listen(port, () => {
---
+++ src/config/db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { config } from "./config.js";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log(`Database connection successful`);
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.log(`Error in connecting to database, ${err}`);
+    });
+
+    await mongoose.connect(config.databaseURI as string);
+    
+  } catch (error) {
+    console.log(`MongoDB Error: ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
